e9513efeae0f544bc0415a7dc0669ae21cf6b5dd
bearbearbear <bear-bear-bear@kakao.com>
2021-02-05 23:08:52 +0900

    feat: App 클래스 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div>Hello!!!!!</div>;
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  componentDidMount() {
+    const done = () => this.setState({ isLoading: false });
+
+    // test code
+    setTimeout(done, 6000);
+  }
+
+  render() {
+    console.log('render');
+    const { isLoading } = this.state;
+
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+  }
 }
 
 export default App;
