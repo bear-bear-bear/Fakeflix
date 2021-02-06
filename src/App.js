893888a9b3b888a295674ef4ff407fece514bc50
bearbearbear <bear-bear-bear@kakao.com>
2021-02-06 15:32:11 +0900

    feat: 영화 데이터 불러오기 기능 추가,  렌더링 간격 측정 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
+    startTime: Date.now(),
     isLoading: true,
     movies: [],
   };
 
-  componentDidMount() {
-    const done = () => this.setState({ isLoading: false });
+  APIs = {
+    movieList: 'https://yts-proxy.now.sh/list_movies.json',
+  };
 
-    // test code
-    setTimeout(done, 6000);
+  appTimer = () => {
+    const { startTime } = this.state;
+    const endTime = Date.now();
+
+    console.log(`렌더링 시간간격: ${endTime - startTime}ms`);
+
+    this.startTime = endTime; // initial
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(this.APIs.movieList);
+
+    this.setState({
+      movies,
+      isLoading: false,
+    });
+  };
+
+  componentDidMount() {
+    this.getMovies();
   }
 
   render() {
-    console.log('render');
     const { isLoading } = this.state;
+    this.appTimer(); // Time test code
 
     return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
