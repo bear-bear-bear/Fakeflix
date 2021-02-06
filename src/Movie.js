56fbd1dcff8130326982ec68fc4389075d496001
bearbearbear <bear-bear-bear@kakao.com>
2021-02-06 16:36:13 +0900

    feat: 영화 데이터에 장르 추가

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-const Movie = ({ title, year, summary, poster }) => {
+const Movie = ({ title, year, summary, poster, genres }) => {
   return (
     <article class="movie">
       <img src={poster} alt={title} title={title}></img>
@@ -10,6 +10,15 @@ const Movie = ({ title, year, summary, poster }) => {
         <h3 class="movie__title">{title}</h3>
         <h5 class="movie__year">{year}</h5>
         <p class="movie__summary">{summary}</p>
+        <ul class="movie__genres">
+          {genres.map((genre, idx) => {
+            return (
+              <li key={idx} class="movie__genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </article>
   );
@@ -20,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
