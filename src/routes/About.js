2f5c89de536a943c358ab6c6ae098809b9ec54f5
bearbearbear <bear-bear-bear@kakao.com>
2021-02-11 20:53:48 +0900

    feat: About 컴포넌트 기본 스타일링

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+/* css */
+import './About.css';
+
 const About = () => {
-  return <span>About this page: I built it because I love movies.</span>;
+  return (
+    <article className="about__container">
+      <span>
+        "Freedom is th freedom to say that two plus two make four. If that is granted, all else
+        follows."
+      </span>
+      <span>- George Orwell, 1984</span>
+    </article>
+  );
 };
 
 export default About;
