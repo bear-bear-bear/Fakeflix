477eed2cdf09602dbc3ef5c0201af4e85e09cba8
bearbearbear <bear-bear-bear@kakao.com>
2021-02-11 20:34:13 +0900

    feat: 페이지 라우팅
    Home(/)
    About(/about)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 /* modules */
+import { HashRouter, Route } from 'react-router-dom';
 
 /* components */
 import Home from './routes/Home';
+import About from './routes/About';
 
 /* css */
 import './App.css';
 
 const App = () => {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 };
 
 export default App;
