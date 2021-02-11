bb236f970c2af24e372a423265dff27b4eeaa93f
bearbearbear <bear-bear-bear@kakao.com>
2021-02-12 01:02:08 +0900

    feat: movie 컴포넌트의 구조를 detail 컴포넌트에 복사하고 클래스명 변경

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,9 +4,45 @@ import React from 'react';
 /* css */
 import './Detail.css';
 
-const Detail = (props) => {
-  console.log(props);
-  return <article>detail</article>;
-};
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+
+    if (location.state === undefined) {
+      history.push('/');
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+
+    if (!!location.state) {
+      const { title, year, summary, poster, genres } = location.state;
+
+      return (
+        //TODO: 트레일러 영상 추가
+        <article className="detail">
+          <img src={poster} alt={title} title={title}></img>
+          <div className="detail__data">
+            <h3 className="detail__title">{title}</h3>
+            <h5 className="detail__year">{year}</h5>
+            <ul className="detail__genres">
+              {genres.map((genre, idx) => {
+                return (
+                  <li key={idx} className="detail__genre">
+                    {genre}
+                  </li>
+                );
+              })}
+            </ul>
+            <p className="detail__summary">{summary.slice(0, 180)}...</p>
+          </div>
+        </article>
+      );
+    } else {
+      return null;
+    }
+  }
+}
 
 export default Detail;
