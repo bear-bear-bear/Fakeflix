e8def5ee14de3f7a94269acce36a1a137c742cc2
bearbearbear <bear-bear-bear@kakao.com>
2021-02-11 23:42:24 +0900

    fix: 인용구 문구 오류 수정

--- src/routes/About.js
+++ src/routes/About.js
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <article className="about__container">
       <span>
-        "Freedom is th freedom to say that two plus two make four. If that is granted, all else
+        "Freedom is the freedom to say that two plus two make four. If that is granted, all else
         follows."
       </span>
       <span>- George Orwell, 1984</span>
