754380eb2bc85e62a945030de7347acb6726478f
bearbearbear <bear-bear-bear@kakao.com>
2021-02-12 13:57:59 +0900

    feat: 호출되는 영화 개수 증가 (20 > 50)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,7 +20,7 @@ class Home extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?limit=50&sort_by=rating');
 
     this.setState({
       movies: movies,
